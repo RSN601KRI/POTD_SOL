ee6c5c8f58b2fd499acf2a5213f9e9c132fa0b67
Roshni Kumari <106860359+RSN601KRI@users.noreply.github.com>
2023-07-20 16:20:07 +0530

    Added Non Repeating Character

---
+++ July mon Q/S/Non repeating character.py
@@ -0,0 +1,10 @@
+class Solution:
+    
+    #Function to find the first non-repeating character in a string.
+    def nonrepeatingCharacter(self,s):
+        #code here
+        for i in s:
+            if s.count(i)==1:
+                 # Return the first non-repeating character found.
+                return i
+        return '$'
