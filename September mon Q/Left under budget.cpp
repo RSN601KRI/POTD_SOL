54ea19833728d5e5ace60847a3b07590097a5f02
Roshni Kumari <106860359+RSN601KRI@users.noreply.github.com>
2023-09-02 09:34:50 +0530

    created

---
+++ September mon Q/Left under budget.cpp
@@ -0,0 +1,47 @@
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+class Solution
+{
+public:
+    int getCount(Node *root, int k)
+    {
+        int ans = 0;
+        
+        queue<pair<Node *, int>> q;
+        q.push({root, 1});
+        
+        while(!q.empty()){
+            Node * cur;
+            int level;
+            
+            tie(cur, level) = q.front();
+            q.pop();
+            
+            if(cur -> left != nullptr)
+                q.push({cur -> left, level + 1});
+            if(cur -> right != nullptr)
+                q.push({cur -> right, level + 1});
+                
+            if(!cur -> left and !cur -> right){
+                if(level <= k){
+                    k -= level;
+                    ++ans;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
