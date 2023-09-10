ad309265b23343bee09d77e5d88f7a82242c0698
Roshni Kumari <106860359+RSN601KRI@users.noreply.github.com>
2023-09-10 13:36:14 +0530

    created

---
+++ September mon Q/Insert a node in BST.cpp
@@ -0,0 +1,38 @@
+/*
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+    public:
+    Node* insert(Node* node, int data) {
+        function<Node *(Node *)> insert = [&](Node * cur) -> Node * {
+            if(cur == nullptr){
+                Node * newnode = new Node(data);
+                return newnode;
+            }
+            
+            if(cur -> data == data)
+                return cur;
+                
+            if(cur -> data > data)
+                cur -> left = insert(cur -> left);
+            else
+                cur -> right = insert(cur -> right);
+                
+            return cur;
+        };
+        
+        return insert(node);
+    }
+
+};
