2f1668a76725cf373b7ec3efb2b7e4fb24ad9833
Roshni Kumari <106860359+RSN601KRI@users.noreply.github.com>
2024-01-21 21:35:02 -0800

    Add files via upload

---
+++ JanuaryMon/Plant from root with a verified sum.cpp
@@ -0,0 +1,29 @@
+
+/* A Tree node
+struct Node
+{
+	int key;
+	struct Node *left, *right;
+};*/
+
+class Solution
+{
+    public:
+    void dfs(Node* root, int sum, int target,vector<int>&temp,vector<vector<int>>&ans){
+        if(!root) return;
+        temp.push_back(root->key);
+        if(sum+root->key==target){
+            ans.push_back(temp);
+        }
+        dfs(root->left,sum+root->key,target,temp,ans);
+        dfs(root->right,sum+root->key,target,temp,ans);
+        temp.pop_back();
+    }
+    vector<vector<int>> printPaths(Node *root, int sum)
+    {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        dfs(root,0,sum,temp,ans);
+        return ans;
+    }
+};
