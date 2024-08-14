d5b5bf39229e0da9cce0c751e610a492708911c8
Roshni Kumari <106860359+RSN601KRI@users.noreply.github.com>
2024-08-14 18:17:21 +0530

    Add files via upload

---
+++ August mon Q/Longest Common Substring.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int longestCommonSubstr(string str1, string str2) {
+        // your code here
+        int n = str1.size();
+        int m = str2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        int result = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    result = max(result, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        return result;
+    }
+}
