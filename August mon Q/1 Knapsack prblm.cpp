709de814349cd305a7bddc8c142ca6a0fef90d23
Roshni Kumari <106860359+RSN601KRI@users.noreply.github.com>
2024-08-24 10:36:00 +0530

    Add files via upload

---
+++ August mon Q/1 Knapsack prblm.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    // Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, vector<int>& wt, vector<int>& val) {
+        // Your code here
+        int n = wt.size();
+        vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+
+        // Building the dp array in bottom-up manner
+        for (int i = 1; i <= n; i++) {
+            for (int w = 1; w <= W; w++) {
+                if (wt[i - 1] <= w) {
+                    dp[i][w] = max(dp[i - 1][w], val[i - 1] + dp[i - 1][w - wt[i - 1]]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        // The last cell will have the maximum value
+        return dp[n][W];
+    }
+};
